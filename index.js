32ff4b2cd28552afd9cd03aa8460e2b8eee3ce3f
chsuresh3839@gmail.com <chsuresh3839@gmail.com>
2025-03-04 18:20:59 +0530

    Added build script for Render

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ import { authenticate } from './src/middleware/authMiddleware.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({origin: "https://hms-mauve.vercel.app", credentials: true }));
+app.use(cors({origin: ["https://hmsy.vercel.app/"],
+    methods: ["POST","GET","PUT","DELETE"],
+    credentials: true }));
 app.use(express.json());
 
 
